f7f0b6621e768eccbace7f1cb90ad579bbf5095a
qiguan <qiguanjiezl@163.com>
2020-03-28 11:09:49 +0800

    实现登录、登录状态保持、注销、主页等，同时优化导航栏

--- app.py
+++ app.py
@@ -9,8 +9,28 @@ app = Flask(__name__)
 # 从对象中导入config
 app.config.from_object(config)
 
+# 登录状态保持
+@app.context_processor
+def login_status():
+    # 从session中获取email
+    email = session.get('email')
+    # 如果有email信息，则证明已经登录了，我们从数据库中获取登陆者的昵称和用户类型，来返回到全局
+    if email:
+        try:
+            cur = db.cursor()
+            sql = "select nickname,type from UserInformation where email = '%s'" % email
+            db.ping(reconnect=True)
+            cur.execute(sql)
+            result = cur.fetchone()
+            if result:
+                return {'email':email,'nickname':result[0] ,'user_type':result[1]}
+        except Exception as e:
+            raise e
+    # 如果email信息不存在，则未登录，返回空
+    return {}
 
 
+# 主页
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -53,6 +73,42 @@ def register():
                 return redirect(url_for('index'))
         except Exception as e:
             raise e
+
+# 登录页面
+@app.route('/login',methods=['GET','POST'])
+def login():
+    if request.method == 'GET':
+        return render_template('login.html')
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+        if not all([email,password]):
+            flash("请将信息填写完整！")
+            return render_template('login.html')
+        try:
+            cur = db.cursor()
+            sql = "select password from UserInformation where email = '%s'" % email
+            db.ping(reconnect=True)
+            cur.execute(sql)
+            result = cur.fetchone()
+            if result is None:
+                flash("该用户不存在")
+                return render_template('login.html')
+            if check_password_hash(result[0],password):
+                session['email'] = email
+                session.permanent = True
+                return redirect(url_for('index'))
+            else:
+                flash("密码错误！")
+                return render_template('login.html')
+        except Exception as e:
+            raise e
+# 用户注销
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for(('index')))
+
 
 if __name__ == '__main__':
     app.run()
