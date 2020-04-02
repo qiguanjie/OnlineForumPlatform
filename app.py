31a335a3feefd095bf0e370aeafa472dcd2b17bc
qiguan <qiguanjiezl@163.com>
2020-04-02 22:41:12 +0800

    文件上传功能完成

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 from flask import *
 from werkzeug.security import generate_password_hash, check_password_hash
+from werkzeug.utils import secure_filename
 from config import db
 import random
 import time
 import config
+import os
+import re
 from decorators import login_limit
 
 app = Flask(__name__)
@@ -299,6 +302,71 @@ def show_issue():
             raise e
         return render_template('show_issue.html',issue_detail=issue_detail)
 
+# 生成120位随机id
+def gengenerateFno():
+    re = ""
+    for i in range(120):
+        re += chr(random.randint(65, 90))
+    return re
+
+# 资源上传页面
+@app.route('/post_file',methods=['GET','POST'])
+@login_limit
+def post_file():
+    if request.method == 'GET':
+        return render_template('post_file.html')
+    if request.method == 'POST':
+        email = session.get('email')
+        upload_file = request.files.get('file')
+        filename = request.form.get('filename')
+        file_info = request.form.get('file_info')
+        file_path = 'store'
+        file_time = time.strftime("%Y-%m-%d %H:%M:%S")
+        Fno = gengenerateFno()
+        try:
+            cur = db.cursor()
+            sql = "select * from Files where Fno = '%s'" % Fno
+            db.ping(reconnect=True)
+            cur.execute(sql)
+            result = cur.fetchone()
+            # 如果result不为空，即该Fno已存在时，一直生成随机的Fno，只到该数据库中不存在
+            while result is not None:
+                Fno = gengenerateFno()
+                sql = "select * from Files where Fno = '%s'" % Fno
+                db.ping(reconnect=True)
+                cur.execute(sql)
+                result = cur.fetchone()
+            # 获取文件的后缀
+            upload_name = str(upload_file.filename)
+            houzhui = upload_name.split('.')[-1]
+            # 保存在本地的名字为生成的Fno+文件后缀，同时修改Fno的值
+            Fno = Fno+"."+houzhui
+            # 保存文件到我们的服务器中
+            upload_file.save(os.path.join(file_path,Fno))
+            # 将文件信息存储到数据库中
+            sql = "insert into Files(Fno, filename, file_info, file_time,email) VALUES ('%s','%s','%s','%s','%s')" % (Fno,filename,file_info,file_time,email)
+            db.ping(reconnect=True)
+            cur.execute(sql)
+            db.commit()
+            cur.close()
+            return render_template('index.html')
+        except Exception as e:
+            raise e
+
+# 资源专区
+@app.route('/source')
+def source():
+    if request.method == 'GET':
+        try:
+            cur = db.cursor()
+            sql = "select Fno,filename,file_info,file_time,nickname from Files,UserInformation where Files.email = UserInformation.email"
+            db.ping(reconnect=True)
+            cur.execute(sql)
+            files = cur.fetchall()
+            cur.close()
+            return render_template('source.html',files = files)
+        except Exception as e:
+            raise e
 
 
 if __name__ == '__main__':
